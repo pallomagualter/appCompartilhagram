924242f032b74fe9db512db85a0751f288f35bbf
Palloma Gualter <pallomagualter@gmail.com>
2020-08-25 16:59:50 -0300

    second commit

---
+++ src/screens/AddPhoto.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+    Image,
+    Dimensions,
+    Platform,
+    ScrollView,
+    Alert
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+class AddPhoto extends Component {
+    state = {
+        image: null,
+        comment: '',
+    }
+
+    pickImage = () => {
+        ImagePicker.showImagePicker({
+            title: 'Escolha a imagem',
+            maxHeight: 600,
+            maxWidth: 800
+        }, res => {
+            if (!res.didCancel) {
+                this.setState({ image: { uri: res.uri, base64: res.data }})
+            }
+        })
+    }
+
+    save = async () => {
+        Alert.alert('Imagem adicionada!', this.state.comment)
+    }
+}
